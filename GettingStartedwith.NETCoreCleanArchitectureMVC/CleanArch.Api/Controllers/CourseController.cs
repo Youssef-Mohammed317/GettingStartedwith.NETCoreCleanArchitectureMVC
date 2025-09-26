737cb5184ab1bd2bdc5bc02ad6be3f8720836e1f
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2025-09-26 13:25:23 +0300

    Api Project and adding course controller

---
+++ GettingStartedwith.NETCoreCleanArchitectureMVC/CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+
+        private readonly ILogger<CourseController> _logger;
+
+        public CourseController(ILogger<CourseController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
